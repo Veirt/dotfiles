61083b1555b0e175235f661eaa5715f7b73dd08e
ExLog <55097092+ExLog@users.noreply.github.com>
2021-05-13 12:27:44 +0800

    Add some aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,17 +1,21 @@
-# Remove welcome message
-set -U fish_greeting 
+set -U fish_greeting
+function __fish_command_not_found_handler --on-event fish_command_not_found
+    echo "fish: Unknown command '$argv'"
+end
 
 # I use arch btw
 alias pacsyu="sudo pacman -Syu"
 
-alias gs="git status"
+alias bluetooth="bluetoothctl"
 
+alias gs="git status"
+alias ga="git add"
 alias ls="exa"
+alias la="exa -la"
+alias ll="exa -ll"
+
+alias repo="cd ~/Repositories"
+alias dotfiles="cd ~/.dotfiles"
 
 # Starship prompt
-starship init fish | source
-if not set -q TMUX
-    set -g TMUX tmux new-session -d -s base
-    eval $TMUX
-    tmux attach-session -d -t base
-end
+# starship init fish | source
