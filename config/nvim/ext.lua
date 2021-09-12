3cd02eed419f0dfa625de56f5160ad5910554188
Muhammad Dony Mulya <55097092+ExLog@users.noreply.github.com>
2021-09-12 20:45:28 +0800

    nvim: setup treesitter

--- config/nvim/ext.lua
+++ config/nvim/ext.lua
@@ -3,3 +3,12 @@ require('lualine').setup {
 }
 
 require'telescope'.load_extension('project')
+
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = { "python", "rust", "javascript", "typescript", "lua", "fish", "bash", "dockerfile", "graphql",
+    "cpp", "jsdoc", "java", "json", "php", "toml", "vim", "yaml", "html", "css" },
+
+    highlight = { enable = true },
+    indent = { enable = true }
+
+}
