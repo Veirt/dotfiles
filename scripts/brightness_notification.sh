98bfa1b195bcd86497b78c0cfb7f625afebede88
Muhammad Dony Mulya <55097092+ExLog@users.noreply.github.com>
2021-09-13 18:17:32 +0800

    reorganize and add notification script

---
+++ scripts/brightness_notification.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+BRIGHTNESS=$(brightnessctl get);
+MAX_BRIGHTNESS=$(brightnessctl max);
+PCT=$(echo $BRIGHTNESS $MAX_BRIGHTNESS  | awk '{printf "%4.0f\n",($1/$2)*100}' | tr -d '[:space:]')
+
+# Send the notification with the icon:
+notify-send.sh "Brightness ${PCT}%" \
+    --replace-file=/tmp/brightness-notification \
+    -t 2000 \
+    --icon ~/.icons/brightness.png \
+    -h int:value:${PCT} \
+    -h string:synchronous:brightness-change
