7491b6377ddfd7dfb4178587076d8079069b20b0
Muhammad Dony Mulya <55097092+ExLog@users.noreply.github.com>
2021-09-18 11:05:58 +0800

    tmux: remove line numbers on tmux-cht.sh

--- scripts/tmux-cht.sh
+++ scripts/tmux-cht.sh
@@ -6,9 +6,9 @@ query=`echo $query | tr ' ' '+'`
 
 if grep -qs "$selected" ~/.tmux-cht-languages; then
     echo "curl cht.sh/$selected/$query"
-    tmux neww bash -c "curl -s cht.sh/$selected/$query | bat --paging=always"
+    tmux neww bash -c "curl -s cht.sh/$selected/$query | bat --paging=always --style=plain"
 else
     echo "curl cht.sh/$selected~$query"
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | bat --paging=always"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | bat --paging=always --style=plain"
 fi
 
