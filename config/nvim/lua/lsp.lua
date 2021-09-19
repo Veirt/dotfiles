e1b28218d20aae964da5f7e0fa17462369c74981
Muhammad Dony Mulya <55097092+ExLog@users.noreply.github.com>
2021-09-19 12:43:10 +0800

    nvim: disable lsp virtual text

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -1,6 +1,13 @@
 local nvim_lsp = require('lspconfig')
 local coq = require('coq')
 
+-- Disable annoying error messages
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false
+    }
+)
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
