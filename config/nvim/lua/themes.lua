07fcb16034804064a0cb3f418d02e57e45354878
Muhammad Dony Mulya <55097092+ExLog@users.noreply.github.com>
2021-09-21 15:25:56 +0800

    oh god i love tokyonight

--- config/nvim/lua/themes.lua
+++ config/nvim/lua/themes.lua
@@ -1,41 +1,46 @@
-require('catppuccino').setup {
-    colorscheme = "dark_catppuccino",
-    transparency = false,
-    term_colors = false,
-    integrations = {
-        treesitter = true,
-        native_lsp = {
-            enabled = true,
-            virtual_text = {
-                errors = "italic",
-                hints = "italic",
-                warnings = "italic",
-                information = "italic",
-            },
-            underlines = {
-                errors = "underline",
-                hints = "underline",
-                warnings = "underline",
-                information = "underline",
-            }
-        },
-        lsp_trouble = true,
-        lsp_saga = false,
-        gitsigns = true,
-        telescope = true,
-        nvimtree = {
-            enabled = true,
-            show_root = true,
-        },
-        which_key = true,
-        dashboard = true,
-        bufferline = true,
-        markdown = false,
-        lightspeed = false,
-        ts_rainbow = false,
-        hop = false,
-    }
+-- require('catppuccino').setup {
+--     colorscheme = "dark_catppuccino",
+--     transparency = false,
+--     term_colors = false,
+--     integrations = {
+--         treesitter = true,
+--         native_lsp = {
+--             enabled = true,
+--             virtual_text = {
+--                 errors = "italic",
+--                 hints = "italic",
+--                 warnings = "italic",
+--                 information = "italic",
+--             },
+--             underlines = {
+--                 errors = "underline",
+--                 hints = "underline",
+--                 warnings = "underline",
+--                 information = "underline",
+--             }
+--         },
+--         lsp_trouble = true,
+--         lsp_saga = false,
+--         gitsigns = true,
+--         telescope = true,
+--         nvimtree = {
+--             enabled = true,
+--             show_root = true,
+--         },
+--         which_key = true,
+--         dashboard = true,
+--         bufferline = true,
+--         markdown = false,
+--         lightspeed = false,
+--         ts_rainbow = false,
+--         hop = false,
+--     }
 
-}
+-- }
 
-vim.cmd[[colorscheme catppuccino]]
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_hide_inactive_statusline = true
+
+
+-- vim.cmd[[colorscheme catppuccino]]
+vim.cmd[[colorscheme tokyonight]]
