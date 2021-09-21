e65f9f3ab687e2836fc809a38a8579b853489bf6
Muhammad Dony Mulya <55097092+ExLog@users.noreply.github.com>
2021-09-21 10:30:20 +0800

    tmux: no more error

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,23 +3,26 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    items=`find ~/Repositories -maxdepth 1 -mindepth 1 -type d`
-    items+="\n$HOME/.dotfiles"
-    selected=`echo -e "$items" | fzf`
+    selected=$(find ~/Repositories -mindepth 1 -maxdepth 1 -type d -or -type l | fzf)
 fi
 
-# check if selected is not empty
-if [ -z $selected ]; then
+if [[ -z $selected ]]; then
     exit 0
-else
-    echo "selected: $selected"
-    tmux_session_name=`basename $selected | tr . _`
-
-    tmux switch-client -t $tmux_session_name
-    if [[ $? -eq 0 ]]; then
-        exit 0
-    fi
-
-    tmux new-session -c $selected -d -s $tmux_session_name && tmux switch-client -t $tmux_session_name || tmux new -c $selected -A -s $tmux_session_name
 fi
 
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
+    exit 0
+fi
+
+if ! tmux has-session -t $selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
+
+if [[ -z $TMUX ]]; then
+    tmux attach-session -t $selected_name
+else
+    tmux switch-client -t $selected_name
+fi
