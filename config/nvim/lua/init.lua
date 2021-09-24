a966a92a4467de895998d14eba2cc0b4c7b43f57
Muhammad Dony Mulya <55097092+ExLog@users.noreply.github.com>
2021-09-24 16:51:49 +0800

    feat(nvim): customize bufferline on nvimtree

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -9,9 +9,19 @@ require("trouble").setup{
 require('colorizer').setup()
 
 require("bufferline").setup{
-  options = {
-    diagnostics = "nvim_lsp",
-    show_buffer_close_icons = false
+    options = {
+        diagnostics = "nvim_lsp",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                highlight = "Directory",
+                text_align = "left"
+            }
+        }
+
   }
 }
 
