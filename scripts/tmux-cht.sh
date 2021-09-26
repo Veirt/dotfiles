95c7af5ddd02ef6102bcb21c453cbb1f5d7a759e
Muhammad Dony Mulya <55097092+ExLog@users.noreply.github.com>
2021-09-26 12:56:14 +0800

    feat(script): print query if not selected

--- scripts/tmux-cht.sh
+++ scripts/tmux-cht.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf --print-query`
 echo "Selected: $selected"
 read -p "Enter Query: " query
 query=`echo $query | tr ' ' '+'`
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
-    echo "curl cht.sh/$selected/$query"
-    tmux neww bash -c "curl -s cht.sh/$selected/$query | bat --paging=always --style=plain"
-else
+if grep -qs "$selected" ~/.tmux-cht-command; then
     echo "curl cht.sh/$selected~$query"
     tmux neww bash -c "curl -s cht.sh/$selected~$query | bat --paging=always --style=plain"
+else
+    echo "curl cht.sh/$selected/$query"
+    tmux neww bash -c "curl -s cht.sh/$selected/$query | bat --paging=always --style=plain"
 fi
 
