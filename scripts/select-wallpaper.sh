fe5e038b27f0575d65d82b6c974f74990964c283
Muhammad Dony Mulya <vveirt@gmail.com>
2021-10-01 18:22:18 +0800

    feat(i3): migrate to i3

--- scripts/select-wallpaper.sh
+++ scripts/select-wallpaper.sh
@@ -2,6 +2,7 @@
 
 SELECTED=$(find $HOME/Pictures/Wallpapers -mindepth 1 -maxdepth 1 | fzf)
 
-echo -e "set \$background '${SELECTED}'\noutput * bg \$background fill" &> $HOME/.config/sway/conf.d/wallpaper
+# sway
+# echo -e "set \$background '${SELECTED}'\noutput * bg \$background fill" &> $HOME/.config/sway/conf.d/wallpaper
 
 
