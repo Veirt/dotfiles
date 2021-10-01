486f7fa431a56dc8e913e7b82e3563f24b7bee6c
Muhammad Dony Mulya <vveirt@gmail.com>
2021-10-01 15:15:33 +0800

    fix(script): fix broken print query

--- scripts/tmux-cht.sh
+++ scripts/tmux-cht.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf --print-query`
+
+selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf --print-query | tail -1 | xargs -r`
 echo "Selected: $selected"
 read -p "Enter Query: " query
 query=`echo $query | tr ' ' '+'`
 
 if grep -qs "$selected" ~/.tmux-cht-command; then
-    echo "curl cht.sh/$selected~$query"
+    # echo "curl cht.sh/$selected~$query" | less
     tmux neww bash -c "curl -s cht.sh/$selected~$query | bat --paging=always --style=plain"
 else
-    echo "curl cht.sh/$selected/$query"
+    # echo "curl cht.sh/$selected/$query" | less
     tmux neww bash -c "curl -s cht.sh/$selected/$query | bat --paging=always --style=plain"
 fi
 
