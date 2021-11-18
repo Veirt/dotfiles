0a7efec7c8c92b46720032ebfde5bfa185071e22
Muhammad Dony Mulya <vveirt@gmail.com>
2021-11-18 16:11:22 +0800

    refactor(nvim): remove formatter.nvim

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -47,7 +47,6 @@ return require("packer").startup(function()
 	use({ "ThePrimeagen/harpoon", requires = { "nvim-lua/popup.nvim" } }) -- Get you where you want with the fewest keystrokes.
 	use("lewis6991/gitsigns.nvim") -- Super fast git decorations
 	use("sbdchd/neoformat") -- Format code/Prettier
-	use("mhartington/formatter.nvim")
 	use({ "tami5/lspsaga.nvim", branch = "nvim51" }) -- LSP plugin with highly performant UI
 	use("ray-x/lsp_signature.nvim")
 	use("RishabhRD/popfix")
