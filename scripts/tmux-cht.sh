ddfe08bc5a02fc77c07d146b29f54b0b93b414b7
Muhammad Dony Mulya <vveirt@gmail.com>
2021-12-16 15:05:49 +0800

    refactor(script): move tmux-cht related files to .tmux dir

--- scripts/tmux-cht.sh
+++ scripts/tmux-cht.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf --print-query | tail -1 | xargs -r`
+selected=$(cat ~/.tmux/tmux-cht-languages ~/.tmux/tmux-cht-command | fzf --print-query | tail -1 | xargs -r)
 echo "Selected: $selected"
 read -p "Enter Query: " query
-query=`echo $query | tr ' ' '+'`
+query=$(echo $query | tr ' ' '+')
 
-if grep -qs "$selected" ~/.tmux-cht-command; then
-    # echo "curl cht.sh/$selected~$query" | less
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | bat --paging=always --style=plain"
+if grep -qs "$selected" ~/.tmux/tmux-cht-command; then
+	# echo "curl cht.sh/$selected~$query" | less
+	tmux neww bash -c "curl -s cht.sh/$selected~$query | bat --paging=always --style=plain"
 else
-    # echo "curl cht.sh/$selected/$query" | less
-    tmux neww bash -c "curl -s cht.sh/$selected/$query | bat --paging=always --style=plain"
+	# echo "curl cht.sh/$selected/$query" | less
+	tmux neww bash -c "curl -s cht.sh/$selected/$query | bat --paging=always --style=plain"
 fi
-
