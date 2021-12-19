fc4ee9516c1bcd18617ef63713d10f3f9dcd4957
Muhammad Dony Mulya <vveirt@gmail.com>
2021-12-19 15:06:37 +0800

    refactor(fish): clean up fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,27 +7,9 @@ bind \cf 'tmux-sessionizer.sh'
 bind -M insert \cr 'ranger'
 bind \cr 'ranger'
 
-function __fish_command_not_found_handler --on-event fish_command_not_found
-    echo "fish: Unknown command '$argv'"
-end
-
-function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
-    if test "$argv" = !!
-    eval command sudo $history[1]
-else
-    command sudo $argv
-    end
-end
-
 source ~/.shell_aliases
-
-set -a FZF_DEFAULT_OPTS --bind alt-j:down,alt-k:up
-if type fd &> /dev/null
-    export FZF_DEFAULT_COMMAND='fd --type f'
-end
 
 # Starship prompt
 if type starship &> /dev/null
     starship init fish | source
 end
-
---
+++ config/fish/functions/__fish_command_not_found_handler.fish
@@ -0,0 +1,3 @@
+function __fish_command_not_found_handler --on-event fish_command_not_found
+    echo "fish: Unknown command '$argv'"
+end
