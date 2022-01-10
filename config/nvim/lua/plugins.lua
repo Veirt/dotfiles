f810b0a6474c629809896ef871ee49672d772f6d
Muhammad Dony Mulya <vveirt@gmail.com>
2022-01-10 18:57:28 +0800

    feat(nvim): compile when plugin.lua changes

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,3 +1,10 @@
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup({
