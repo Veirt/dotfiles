70052ff26b0f0edf015c13729f29d493d28ddeb2
Don <vveirt@gmail.com>
2022-01-14 00:46:33 +0800

    feat(nvim): update prisma treesitter

--- config/nvim/lua/treesitter.lua
+++ config/nvim/lua/treesitter.lua
@@ -1,13 +1,5 @@
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
-parser_config.prisma = {
-    install_info = {
-        url = "https://github.com/richin13/tree-sitter-prisma.git", -- temporarily :D
-        files = { "src/parser.c" },
-    },
-    filetype = "prisma",
-}
-
 require("nvim-treesitter.configs").setup({
     ensure_installed = {
         "python",
