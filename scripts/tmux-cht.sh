d4f825a5a37ae97ecd109e1d291ac898ee1705b2
Veirt <vveirt@gmail.com>
2022-02-02 17:57:01 +0800

    fix(tmux): correct path

--- scripts/tmux-cht.sh
+++ scripts/tmux-cht.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-selected=$(cat ~/.tmux/tmux-cht-languages ~/.tmux/tmux-cht-command | fzf --print-query | tail -1 | xargs -r)
+selected=$(cat ~/.config/tmux/tmux-cht-languages ~/.config/tmux/tmux-cht-command | fzf --print-query | tail -1 | xargs -r)
 echo "Selected: $selected"
 read -p "Enter Query: " query
 query=$(echo $query | tr ' ' '+')
 
-if grep -qs "$selected" ~/.tmux/tmux-cht-command; then
+if grep -qs "$selected" ~/.config/tmux/tmux-cht-command; then
     # echo "curl cht.sh/$selected~$query" | less
     tmux neww bash -c "curl -s cht.sh/$selected~$query | bat --paging=always --style=plain"
 else
