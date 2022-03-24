dc586f995d771a605ad24e03b70537149188c384
Veirt <vveirt@gmail.com>
2022-03-25 06:24:59 +0800

    fix: deprecated nightfox

--- config/nvim/lua/themes/nightfox.lua
+++ config/nvim/lua/themes/nightfox.lua
@@ -1,17 +1,18 @@
 local nightfox = require("nightfox")
 
 nightfox.setup({
-    fox = "nordfox",
-    transparent = true,
-    terminal_colors = true,
-    styles = {
-        comments = "italic",
-        keywords = "bold",
-        functions = "italic,bold",
-    },
-    inverse = {
-        match_paren = true, -- inverse the highlighting of match_parens
+    options = {
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+            comments = "italic",
+            keywords = "bold",
+            functions = "italic,bold",
+        },
+        inverse = {
+            match_paren = true, -- inverse the highlighting of match_parens
+        },
     },
 })
 
-nightfox.load()
+vim.cmd([[colorscheme nordfox]])
