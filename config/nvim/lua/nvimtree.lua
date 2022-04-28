b6e77e3489b9ec821a0238ceafd6d5be60dd5554
Veirt <vveirt@gmail.com>
2022-04-28 15:41:12 +0800

    fix(nvim): deprecated nvim-tree config

--- config/nvim/lua/nvimtree.lua
+++ config/nvim/lua/nvimtree.lua
@@ -14,16 +14,14 @@ g.nvim_tree_icons = {
 }
 
 require("nvim-tree").setup({
-    nvim_tree_indent_markers = 1,
     disable_netrw = true,
     hijack_netrw = true,
     open_on_setup = false,
     ignore_ft_on_setup = {},
-    auto_close = false,
     open_on_tab = false,
     hijack_cursor = false,
     update_cwd = true,
-    update_to_buf_dir = {
+    hijack_directories = {
         enable = true,
         auto_open = true,
     },
@@ -54,12 +52,24 @@ require("nvim-tree").setup({
         ignore = false,
         timeout = 500,
     },
+    renderer = {
+        indent_markers = {
+            enable = false,
+            icons = {
+                corner = "└ ",
+                edge = "│ ",
+                none = "  ",
+            },
+        },
+        icons = {
+            webdev_colors = true,
+        },
+    },
     view = {
         width = 30,
         height = 30,
         hide_root_folder = true,
         side = "left",
-        auto_resize = true,
         mappings = {
             custom_only = false,
             list = {},
