d982860e2597504ab0f0855d6216bd89a05e1840
Veirt <vveirt@gmail.com>
2022-10-18 15:59:58 +0800

    deprecateddeprecated

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -58,7 +58,7 @@ local on_attach = function(client, bufnr)
     end
 end
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local servers = {
