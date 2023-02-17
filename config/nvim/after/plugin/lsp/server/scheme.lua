7bb45044273096646c18e6e9de47382bc01e4740
Veirt <vveirt@gmail.com>
2023-02-17 23:06:57 +0800

    nvim: scheme autopair

--- config/nvim/after/plugin/lsp/server/scheme.lua
+++ config/nvim/after/plugin/lsp/server/scheme.lua
@@ -1,2 +1,8 @@
 local lsp = require("lsp-zero")
 lsp.configure("racket_langserver")
+
+vim.api.nvim_command([[augroup AutoPairScheme]])
+vim.api.nvim_command(
+    [[autocmd! FileType scheme lua require("nvim-autopairs").setup({ map_cr = true, disable_in_macro = true, enable_check_bracket_line = false })]]
+)
+vim.api.nvim_command([[augroup END]])
--- config/nvim/lua/config/on_attach.lua
+++ config/nvim/lua/config/on_attach.lua
@@ -30,7 +30,7 @@ local M = function(client, bufnr)
 
     if client.supports_method("textDocument/formatting") then
         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
+        autocmd("BufWritePre", {
             group = augroup,
             buffer = bufnr,
             callback = function()
