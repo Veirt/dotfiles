09e208def2a549966218cb41c59b22529f80e525
Veirt <vveirt@gmail.com>
2023-05-10 14:57:44 +0800

    fish: web-search things

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,3 +14,7 @@ source ~/.config/shell_aliases
 alias google="web-search google"
 alias github="web-search github"
 alias stackoverflow="web-search stackoverflow"
+
+set -gx WEB_SEARCH_flac 'https://nyaa.si/?f=0&c=2_1&q='
+set -gx WEB_SEARCH_anime 'https://nyaa.si/?f=0&c=1_2&q='
+set -gx WEB_SEARCH_dict 'https://dictionary.cambridge.org/dictionary/english/'
