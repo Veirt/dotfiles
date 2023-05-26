7f54dd96347c9c7a29debe08a1810b339c2b159d
Veirt <vveirt@gmail.com>
2023-05-26 20:16:08 +0800

    fish: web-search config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,11 +10,13 @@ bind \cr 'ranger'
 
 source ~/.config/shell_aliases
 
-# fish only aliases
-alias google="web-search google"
-alias github="web-search github"
-alias stackoverflow="web-search stackoverflow"
-
 set -gx WEB_SEARCH_flac 'https://nyaa.si/?f=0&c=2_1&q='
 set -gx WEB_SEARCH_anime 'https://nyaa.si/?f=0&c=1_2&q='
 set -gx WEB_SEARCH_dict 'https://dictionary.cambridge.org/dictionary/english/'
+set -gx WEB_SEARCH_hi10 'https://hi10anime.com/?s='
+
+alias google="web-search google"
+alias github="web-search github"
+alias stackoverflow="web-search stackoverflow"
+alias anime="web-search anime"
+alias hi10="web-search hi10"
