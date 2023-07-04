1d91191338c491ebc6d7cb282bbeba96ce46b1e9
Veirt <vveirt@gmail.com>
2023-07-04 11:25:07 +0800

    nvim: change theme and add diagnostic icons

--- nvim/.config/nvim/lua/themes/init.lua
+++ nvim/.config/nvim/lua/themes/init.lua
@@ -4,5 +4,11 @@ vim.api.nvim_command([[au ColorScheme * hi LineNr guibg=NONE ctermbg=NONE guifg=
 vim.api.nvim_command([[au ColorScheme * hi CursorLineNr guifg=#fefefe]])
 vim.api.nvim_command([[au ColorScheme * hi NvimTreeNormal guibg=NONE]])
 vim.api.nvim_command([[augroup END]])
+vim.cmd([[
+    sign define DiagnosticSignError text=  linehl= texthl=DiagnosticSignError numhl=
+    sign define DiagnosticSignWarn text=  linehl= texthl=DiagnosticSignWarn numhl=
+    sign define DiagnosticSignInfo text=  linehl= texthl=DiagnosticSignInfo numhl=
+    sign define DiagnosticSignHint text=  linehl= texthl=DiagnosticSignHint numhl=
+]])
 
-require("themes.base16")
+require("themes.solarized")
