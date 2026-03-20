f51cf0dc5a596ff67115c391830804b655c71809
Veirt <veirt@duck.com>
2026-03-20 16:08:12 +0800

    feat(nvim): replace copilot with supermaven

---
+++ nvim/.config/nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,12 @@
+local utils = require("utils")
+
+return {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+        require("supermaven-nvim").setup({
+            keymaps = {
+                accept_suggestion = "<C-e>",
+            },
+        })
+    end,
+}
